a181bdefddb80fa7efb8af34a68de215ffc3f377
Kevin Gay <kevin.gay@epitech.eu>
2017-02-15 02:44:41 -0800

    First Version

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Main function to initiate the program
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        boolean isWorking = true;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Grid Area Counter ");
+        while (isWorking) {
+            System.out.print("Enter the number of the Grid : ");
+            if (sc.hasNextInt()) {
+                readerFile rf = new readerFile(sc.nextInt());
+                sc.nextLine();
+                if (rf.getStr() != null) {
+                    rf.showGrid();
+                    findOInGrid(rf, 1);
+                    rf.showArea();
+                }
+            }
+        }
+    }
+
+    /**
+     * Find all O inside the Grid
+     * @param rf readFile to get the Grid
+     * @param counterArea To know which area we are in
+     */
+    public static void findOInGrid(readerFile rf, int counterArea) {
+        int x = 0;
+        int y = 0;
+
+        for (String aStr : rf.getStr()) {
+            y = 0;
+            for (char c : aStr.toCharArray()) {
+                if (c == 'o') {
+                    findOInGrid(findArea(rf, x, y, counterArea++), counterArea++);
+                    return;
+                }
+                y++;
+            }
+            x++;
+        }
+    }
+
+    /**
+     * Find the O near the area
+     * @param rf ReadFile to get the grid
+     * @param x is the index of the grid
+     * @param y is the index of the grid
+     * @param counterArea is the area number
+     * @return the readFile
+     */
+    public static readerFile findArea(readerFile rf, int x, int y, int counterArea) {
+        if (x < 0 || x > 7 || y < 0 || y > 7)
+            return rf;
+
+        char[] str = rf.getStr()[x].toCharArray();
+
+        if (str[y] == 'o') {
+            if (rf.getarea().get(counterArea) == null)
+                rf.getarea().put(counterArea, 1);
+            else
+                rf.getarea().put(counterArea, rf.getarea().get(counterArea) + 1);
+        } else { return rf; }
+
+
+        str[y] = '#';
+        rf.getStr()[x] = String.valueOf(str);
+        findArea(rf, x, y + 1, counterArea);
+        findArea(rf, x + 1, y, counterArea);
+
+        return rf;
+    }
+}
---
+++ src/readerFile.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by thieg on 15/02/2017.
+ */
+class readerFile {
+
+    private String[] str = new String[8];
+    private Map<Integer, Integer> area = new HashMap<Integer, Integer>();
+
+    /**
+     * Initiate the grid from a file
+     * @param nbr is the number of the file
+     */
+    readerFile(int nbr){
+        File file = new File("grid" + nbr + ".txt");
+        int i = 0;
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+              str[i++] = sc.nextLine();
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("We didnt find your file");
+            this.str = null;
+        }
+    }
+
+    /**
+     * Show the grid
+     */
+    void showGrid() {
+        for (String aStr : str) {
+            for (char c : aStr.toCharArray())
+                System.out.print(c);
+            System.out.println();
+        }
+    }
+
+    /**
+     * Show each area and how many O has been found
+     */
+    void showArea() {
+
+        for (int i = 1; i <= area.size(); i++){
+            System.out.println("Area " + i + " : " + area.get(i));
+        }
+    }
+
+    /**
+     * Get the Area
+     * @return Map of integer for the area
+     */
+    Map<Integer, Integer> getarea(){
+        return this.area;
+    }
+
+    /**
+     * Return the grid inside a String[]
+     * @return the grid
+     */
+    String[] getStr() {
+        return this.str;
+    }
+}
